22a079209b9e184d1284aac4561da1ede2e3661e
Brandon Fu <zhurai.tsuki@gmail.com>
2014-03-03 23:30:22 -0800

    added everything up to now (it works so far, might add more stuff later...) (e.g. more people, place for tax, etc)

---
+++ Assets/Scripts/Button.Quit/QuitButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitButton : MonoBehaviour {
+
+	public void QuitButtonNotify()
+	{
+		Debug.Log("Quit Button Clicked");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/Program Logic/TipCalculator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipCalculator : MonoBehaviour {
+	public float bill;
+	public float tippct;
+	public float tip;
+	public float total;
+	
+	public GameObject objbill;
+	public GameObject objtippct;
+	public GameObject objtip;
+	public GameObject objtotal;
+
+
+	public void Update()
+	{
+		// Get the required Labels
+		UILabel labelbill = objbill.GetComponent<UILabel>();
+		UILabel labeltippct = objtippct.GetComponent<UILabel>();
+		UILabel labeltip = objtip.GetComponent<UILabel>();
+		UILabel labeltotal = objtotal.GetComponent<UILabel>();
+
+		// calculation
+		bill = float.Parse (labelbill.text);
+		tippct = float.Parse (labeltippct.text);
+		tip = tippct * bill;
+		total = tip + bill;
+
+		// update totals
+		labeltip.text = tip.ToString("C");
+		labeltotal.text = total.ToString("C");
+
+	}
+}
